54eb6d6b10752cbca94182caf39bb0f730e83529
Vladimir Panchenko <dropsonic.pan@gmail.com>
2024-09-21 18:29:36 +0200

    Add RGB Matrix highlighting for layers

--- keymap.c
+++ keymap.c
@@ -20,6 +20,27 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
+typedef union {
+    uint32_t raw;
+    struct {
+        bool layer_rgb_override :1; // If activating a layer temporarly turns on RGB Matrix even if it was turned off
+    };
+} user_config_t;
+
+user_config_t user_config; // Persistent config that is saved to EEPROM
+
+static bool rgb_matrix_was_disabled = false;
+
+struct rgb_t {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+enum custom_keycodes {
+    CST_RGBOVRRD = SAFE_RANGE,
+};
+
 enum layers {
     MAC_BASE,
     MAC_FN,
@@ -30,6 +51,24 @@ enum layers {
 	WIN_RGB2,
 	WIN_RGB3,
 	WIN_RGB4,
+};
+
+// clang-format off
+const struct rgb_t layer_rgb_map[] = {
+	[WIN_FN]     = { RGB_YELLOW },
+	[WIN_NUMPAD] = { RGB_BLUE },
+};
+
+// clang-format off
+const struct rgb_t key_rgb_map[] = {
+	[KC_SYSTEM_SLEEP] = { RGB_RED },
+	[C(G(KC_LEFT))]   = { RGB_BLUE },
+	[C(G(KC_RIGHT))]  = { RGB_BLUE },
+	[KC_MPLY]         = { RGB_AZURE },
+	[KC_MPRV]         = { RGB_AZURE },
+	[KC_MNXT]         = { RGB_AZURE },
+	[CST_RGBOVRRD]    = { RGB_PURPLE },
+    [RGB_TOG]         = { RGB_PURPLE },
 };
 
 // clang-format off
@@ -60,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_FN] =   LAYOUT_tkl_ansi(
         _______,                KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    RGB_RMOD,      RGB_MOD,         RGB_TOG,
-        _______,                BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,       KC_SYSTEM_WAKE,  _______,
+        _______,                BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,       KC_SYSTEM_WAKE,  CST_RGBOVRRD,
         RGB_TOG,                RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    KC_CALC,    _______,       KC_SYSTEM_SLEEP, _______,
         _______,                RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,
         _______,                          _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,                   _______,
@@ -122,6 +161,11 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif // ENCODER_MAP_ENABLE
 
+void keyboard_post_init_user(void) {
+  // Read the user config from EEPROM
+  user_config.raw = eeconfig_read_user();
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_keychron_common(keycode, record)) {
         return false;
@@ -134,7 +178,84 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				return false; // Skip default key processing for this keycode
 			}
 			break;
+		case RGB_TOG:
+			if (record->event.pressed) {
+				if (rgb_matrix_was_disabled) {
+					rgb_matrix_was_disabled = false;
+					rgb_matrix_disable_noeeprom();
+				}
+			}
+			break;
+		case CST_RGBOVRRD:
+			if (record->event.pressed) {
+				user_config.layer_rgb_override ^= 1; // Toggles the status
+				eeconfig_update_user(user_config.raw); // Writes the new status to EEPROM
+			}
+			return false;
+			break;
 	}
 	
 	return true;  // Process all other keycodes normally
+}
+
+// Highlight layers
+uint8_t get_rgb_layer(layer_state_t state) {
+	if (get_highest_layer(state) > 0) {
+		const uint8_t layer = get_highest_layer(state);
+		
+		if (layer < (sizeof(layer_rgb_map) / sizeof(layer_rgb_map[0])) && (layer_rgb_map[layer].r > 0 || layer_rgb_map[layer].g > 0 || layer_rgb_map[layer].b > 0)) {
+			return layer;
+		}
+	}
+	
+	return 0;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	if (user_config.layer_rgb_override > 0 && get_rgb_layer(state) > 0) {
+		if (!rgb_matrix_is_enabled()) {
+			rgb_matrix_was_disabled = true;  // Track that it was off
+			rgb_matrix_enable_noeeprom();    // Enable RGB Matrix
+		}
+	} else {
+		// If RGB Matrix was originally off, disable it again
+		if (rgb_matrix_was_disabled) {
+			rgb_matrix_disable_noeeprom();
+			rgb_matrix_was_disabled = false;  // Reset the tracking variable
+		}
+	}
+	
+	return state;
+}
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+	const uint8_t layer = get_rgb_layer(layer_state);
+	
+	if (layer > 0) {		
+		const struct rgb_t layer_color = layer_rgb_map[layer];
+		
+		for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+			for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+				uint8_t index = g_led_config.matrix_co[row][col];
+
+				if (index >= led_min && index < led_max && index != NO_LED) {
+					const uint16_t key = keymap_key_to_keycode(layer, (keypos_t){col,row});
+					
+					if (key > KC_TRNS) {
+						struct rgb_t key_color = layer_color;
+						
+						if (key < (sizeof(key_rgb_map) / sizeof(key_rgb_map[0])) && (key_rgb_map[key].r > 0 || key_rgb_map[key].g > 0 || key_rgb_map[key].b > 0)) {
+							key_color = key_rgb_map[key];
+						}
+						
+						rgb_matrix_set_color(index, key_color.r, key_color.g, key_color.b);
+					} else {
+						rgb_matrix_set_color(index, RGB_OFF);
+					}
+				}
+			}
+		}
+	}
+		
+    return false;
 }
