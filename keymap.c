d478c0666532dfafa4b6ead1cb2d6a89f2a87ebb
Vladimir Panchenko <dropsonic.pan@gmail.com>
2026-06-23 17:00:23 +0200

    Swap macOS Pause and Scroll Lock actions

--- keymap.c
+++ keymap.c
@@ -112,8 +112,8 @@ const struct rgb_t key_rgb_map[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
-        //                                                                                                                          Encoder   Prt Scr   Scroll Lock   Pause
-        KC_ESC,                 KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,             KC_F8,             KC_F9,             KC_F10,           KC_F11,   KC_F12,     KC_MPLY,  MAC_PSCR, MAC_MIC_MUTE, MAC_PTT,
+        //                                                                                                                          Encoder   Prt Scr   Pause     Scroll Lock
+        KC_ESC,                 KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,             KC_F8,             KC_F9,             KC_F10,           KC_F11,   KC_F12,     KC_MPLY,  MAC_PSCR, MAC_PTT, MAC_MIC_MUTE,
         KC_GRV,                 KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     LT(MAC_RGB1, KC_7), LT(MAC_RGB2, KC_8), LT(MAC_RGB3, KC_9), LT(MAC_RGB4, KC_0), KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,       KC_HOME,   KC_PGUP,
         LT(MAC_NUMPAD, KC_TAB), KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,              KC_I,              KC_O,              KC_P,              KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,       KC_END,    KC_PGDN,
         MAC_CAPS,               KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
