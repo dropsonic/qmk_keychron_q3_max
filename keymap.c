707d38f150025589de4abc3e5575e64218a64cf3
Vladimir Panchenko <dropsonic.pan@gmail.com>
2026-06-29 13:53:06 +0200

    Use fine volume steps on macOS encoder

--- keymap.c
+++ keymap.c
@@ -235,7 +235,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [MAC_BASE]   = {ENCODER_CCW_CW(KC_VOLD,     KC_VOLU)},
+    [MAC_BASE]   = {ENCODER_CCW_CW(S(A(KC_VOLD)), S(A(KC_VOLU)))},
     [MAC_FN]     = {ENCODER_CCW_CW(G(KC_Z),     LSG(KC_Z))},
     [WIN_BASE]   = {ENCODER_CCW_CW(KC_VOLD,     KC_VOLU)},
     [WIN_FN]     = {ENCODER_CCW_CW(C(KC_Z),     C(KC_Y))},
